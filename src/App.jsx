9d4991e15d30efcbfc50dd0de7901c2e5c900fe5
Kinshuk Shukla <87925952+Knowledgelocater@users.noreply.github.com>
2025-01-24 16:23:32 +0000

    Landing spelled

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Landing from './components/landing'
+import Landing from './components/Landing'
 import About from './components/About'
 import Navbar from './components/Navbar'    
 import Features from './components/Features'
